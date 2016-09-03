18c48d807e9ac1e06527d2d32fdca829235f36d8
lhr18753412013 <18753412013@163.com>
2016-09-03 20:13:04 +0800

    1
    1

---
+++ Debug/main.pp
@@ -0,0 +1,13 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: D:/ccs/ccsv5/ccs_base/msp430/include/MSP430G2553.h
+main.obj: D:/ccs/ccsv5/ccs_base/msp430/include/in430.h
+main.obj: D:/ccs/ccsv5/tools/compiler/msp430_4.2.1/include/intrinsics.h
+main.obj: C:/Users/Administrator/Desktop/msp430\ program/6_PWM_LED/src/TA_PWM.h
+
+../main.c: 
+D:/ccs/ccsv5/ccs_base/msp430/include/MSP430G2553.h: 
+D:/ccs/ccsv5/ccs_base/msp430/include/in430.h: 
+D:/ccs/ccsv5/tools/compiler/msp430_4.2.1/include/intrinsics.h: 
+C:/Users/Administrator/Desktop/msp430\ program/6_PWM_LED/src/TA_PWM.h: 
---
+++ Debug/src/TA_PWM.pp
@@ -0,0 +1,11 @@
+# FIXED
+
+src/TA_PWM.obj: ../src/TA_PWM.c
+src/TA_PWM.obj: D:/ccs/ccsv5/ccs_base/msp430/include/msp430g2553.h
+src/TA_PWM.obj: D:/ccs/ccsv5/ccs_base/msp430/include/in430.h
+src/TA_PWM.obj: D:/ccs/ccsv5/tools/compiler/msp430_4.2.1/include/intrinsics.h
+
+../src/TA_PWM.c: 
+D:/ccs/ccsv5/ccs_base/msp430/include/msp430g2553.h: 
+D:/ccs/ccsv5/ccs_base/msp430/include/in430.h: 
+D:/ccs/ccsv5/tools/compiler/msp430_4.2.1/include/intrinsics.h: 
